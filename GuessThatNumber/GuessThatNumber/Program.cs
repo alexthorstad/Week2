bab418f489b316edb63033a972a3611044fe962e
SeedPaths-User <lessdismissive@email.com>
2014-09-05 16:21:38 -0600

    9/5 Update @ 4:21 PM

---
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time to guess a number from 1-100.");
+
+            //Generates a random number from 1-100
+            while (true)
+            {   // Assignments
+                int randomNumber = GenerateNumber(1, 101);
+                int counter = 1;
+                while (true)
+                {   //Writes what the parameters/instructions are
+                    Console.Write("Enter a number between 1 and 100(0 to quit):");
+                    int input = Convert.ToInt32(Console.ReadLine());
+
+                    // If input is 0, then the console application will close
+                    if (input == 0)
+                        // Line break
+                        return;
+                        // Random number < text
+                    else if (input < randomNumber)
+                    {
+                        Console.WriteLine("Too low, try again.");
+                        ++counter;
+                        continue;
+                    }
+                        //Random number > text
+                    else if (input > randomNumber)
+                    {
+                        Console.WriteLine("Too high, try again.");
+                        ++counter;
+                        continue;
+                    }
+                    else
+                    {   //If guess is correct, then everything is fine.
+                        Console.WriteLine("You guess it! The number was {0}!", randomNumber);
+                        if (counter == 1)
+                            //Guessing it right away will result in Bonus points
+                            Console.WriteLine("And you guessed it right away! Bonus points!");
+                        else
+                            //Tells you the number of tries to guess correctly
+                            Console.WriteLine("It took you {0} {1}.", counter, counter == 1 ? "try" : "tries");
+                        break;
+                    }
+                }
+            }
+        } //Random number generator with the parameters in place from 1-100(101 of course)
+        static int GenerateNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        private static string returnString;
+        static void Main(string[] args)
+        {
+            string name;
+            // Ask user to enter their name
+            Console.Write("Enter your name: ");
+            // Console reads the name and prints it out
+            name = Console.ReadLine();
+            // Display user greeting
+            Console.WriteLine("Welcome to the Game!");
+            // Game instructions
+            Console.WriteLine("Throughout this game you will be guessing letters of the word until you can guess the word or all of the letters.");
+            Console.ReadLine();
+        }
+        static void hangMan(string[] word)
+        {
+            // Create wordBank list
+            List<string> wordBank = new List<string>() { "Broncos", "Chargers", "Falcons", "Browns", "Patriots" };
+            // Create random word generator
+            Random randGen = new Random();
+
+            // Declare variable to hold lives
+            int lives = 7;
+            // Create container for letters guessed
+            string lettersGuessed = (Console.ReadLine());
+            // Get random string from wordBank
+            string wordtoGuess = wordBank[randGen.Next(1, wordBank.Count)];
+            // User has not won yet
+            bool won = false;
+
+            // Create loop while still playing game
+            while (lives > 0 || !won)
+            {   
+                // Display masked word, number of guesses, letters used
+                Console.WriteLine(maskedWord (wordtoGuess, lettersGuessed));
+                Console.WriteLine("Lives left: " + lives);
+                Console.WriteLine("Letters guessed: " + lettersGuessed);
+                // Get a guess from the user
+
+                // Is guess one character
+                //guess is one character, compare to letters in word to guess
+                //does the letter match?
+                //letter matches, reveal in masked word, subtract a life, add to letters guessed
+                //letter does not match, subtract a life, add to letters guessed
+
+                //guess is not one character, compare to word to guess
+                //does the word match?
+                //the word matches, display the word, congratulate the user
+                //the word does not match, subtract a guess
+                
+            }
+
+            // 
+
+        }
+        //create function for Display Masked Word
+        static string maskedWord(string wordtoGuess, string lettersGuessed)
+        {
+            //get word to guess and letters guessed list
+
+            //declare output
+            returnString = "";
+
+            int i = 0;
+
+            while (i < wordtoGuess.Length)
+            {
+                //get a letter from the word
+                string letter = wordtoGuess[i].ToString();
+                //did the user guess the current letter in the word?
+                if (lettersGuessed.Contains(letter))
+                {
+                    //user guessed the right letter
+                    //add letter to the return string
+                    returnString += letter;
+                }
+                else
+                {
+                    //letter not found
+                    //add an underscore to the return string
+                    returnString += "_";
+                }
+
+                
+            }
+                    //display return string
+                    return returnString;
+
+
+        }
+    }
+}
